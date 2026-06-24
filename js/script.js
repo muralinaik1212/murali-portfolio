9ed279dbb725dfc291241e34eff19a38a6328120
Murali Mahadev Naik <naik89968@gmail.com>
2026-06-24 14:37:59 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
+const mouseLight = document.querySelector(".mouse-light");
+
+document.addEventListener("mousemove", (e)=>{
+
+    mouseLight.style.left = e.clientX + "px";
+    mouseLight.style.top = e.clientY + "px";
+
+});
+
+
 window.addEventListener("load", function(){
 
     setTimeout(function(){
 
-        document.getElementById("loader").style.display = "none";
+        document.getElementById("loader").style.display="none";
 
     },3000);
 
